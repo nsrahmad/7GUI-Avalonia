bee20eab5ad06917fd34af8114a11a628384275b
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-01-04 23:23:06 +0530

    Rename Folder Converters to Converter

---
+++ TemperatureConverter/Converter/CelsiusConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+namespace TemperatureConverter.Converter;
+public class CelsiusConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value != null)
+        {
+            decimal result = ((decimal)value * (9M / 5M)) + 32M;
+            return decimal.Round(result, 1);
+        }
+        return 0m;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value != null)
+        {
+            decimal result = ((decimal)value - 32M) * (5M / 9M);
+            return decimal.Round(result, 1);
+        }
+        return 0m;
+    }
+}
