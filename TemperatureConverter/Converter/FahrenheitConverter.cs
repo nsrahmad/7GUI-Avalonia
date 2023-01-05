a93ad1e4507438bb2b74756a0ee6016930b7b99d
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-01-05 13:09:00 +0530

    Refactor Converter

--- TemperatureConverter/Converter/FahrenheitConverter.cs
+++ TemperatureConverter/Converter/FahrenheitConverter.cs
@@ -4,17 +4,9 @@ using System.Globalization;
 using Avalonia.Data.Converters;
 
 namespace TemperatureConverter.Converter;
-public class CelsiusConverter : IValueConverter
+public class FahrenheitConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value != null)
-        {
-            decimal result = ((decimal)value * (9M / 5M)) + 32M;
-            return decimal.Round(result, 1);
-        }
-        return 0m;
-    }
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value != null ? ((decimal)value * (9M / 5M)) + 32M : 0m;
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
