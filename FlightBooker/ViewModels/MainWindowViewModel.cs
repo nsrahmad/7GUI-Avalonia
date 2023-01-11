7896b02c201b83317a5372bae836bbb9b4dbf295
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-01-11 12:12:31 +0530

    Added Flight Booker example

---
+++ FlightBooker/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace FlightBooker.ViewModels;
+public partial class MainWindowViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    private List<string> flightType = new() { "one-way flight", "return flight" };
+
+    [ObservableProperty]
+    private string selectedFlightType = string.Empty;
+
+    partial void OnSelectedFlightTypeChanged(string value) => ValidateAllProperties();
+
+    private DateTime? flightDate;
+
+    [Required(ErrorMessage = "You must provide a valid flight date.")]
+    public DateTime? FlightDate
+    {
+        get => flightDate;
+        set
+        {
+            _ = SetProperty(ref flightDate, value, true);
+            ValidateProperty(ReturnFlightDate, nameof(ReturnFlightDate));
+        }
+    }
+
+    private DateTime? returnFlightDate;
+
+    [CustomValidation(typeof(MainWindowViewModel), nameof(ValidateReturnFlightDate))]
+    public DateTime? ReturnFlightDate
+    {
+        get => returnFlightDate;
+        set => SetProperty(ref returnFlightDate, value, true);
+    }
+
+    public static ValidationResult ValidateReturnFlightDate(DateTime? returnFlightDate, ValidationContext context)
+    {
+        MainWindowViewModel instance = (MainWindowViewModel)context.ObjectInstance;
+
+        return instance.SelectedFlightType.Equals("one-way flight")
+            ? ValidationResult.Success!
+            : returnFlightDate >= instance.flightDate ? ValidationResult.Success! : (new("The return flight date must be a valid date equal to or after flight date."));
+    }
+
+    [ObservableProperty]
+    private string? message;
+
+    [RelayCommand]
+    private void OnBookClicked() => Message = $"You have booked a {SelectedFlightType} on {FlightDate:dd/MM/yyyy}.";
+
+    [RelayCommand]
+    private void OnClearMessageClicked() => Message = null;
+
+}
---
+++ FlightBooker/Views/MainWindow.axaml.cs
@@ -0,0 +1,7 @@
+using Avalonia.Controls;
+
+namespace FlightBooker.Views;
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+}
