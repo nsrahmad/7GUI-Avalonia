a1fed2194fd2bf0c7791206db6ed610779206eab
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-01-31 17:53:56 +0530

    Moved and renamed file

---
+++ CRUD/Models/ObservableContact.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CRUD.Models;
+
+public partial class ObservableContact : ObservableObject
+{
+    public ObservableContact(string name, string surName)
+    {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    [ObservableProperty]
+    private string name;
+
+    [ObservableProperty]
+    private string surName;
+}
