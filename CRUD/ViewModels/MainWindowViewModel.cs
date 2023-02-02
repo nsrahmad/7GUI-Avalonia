ca69f532b5fa3a21c0bada061261f6f241f71396
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-02-02 13:13:12 +0530

    Implement disposable pattern
    Also clear unused imports.
    Move LoadData to separate class.

--- CRUD/ViewModels/MainWindowViewModel.cs
+++ CRUD/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Reactive.Subjects;
-
-using Avalonia;
-using Avalonia.Platform;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -13,6 +8,8 @@ using CommunityToolkit.Mvvm.Input;
 using CRUD.Models;
 
 using DynamicData;
+
+using static CRUD.Infrastructure.Utils;
 
 namespace CRUD.ViewModels;
 
@@ -32,6 +29,7 @@ public partial class MainWindowViewModel : ObservableObject, IDisposable
     }
 
     private readonly IDisposable list;
+    private bool disposedValue;
     private readonly Subject<Func<ObservableContact, bool>> filter = new();
     private readonly SourceList<ObservableContact> backingContacts = new();
 
@@ -67,20 +65,18 @@ public partial class MainWindowViewModel : ObservableObject, IDisposable
     [ObservableProperty]
     private string filterString = string.Empty;
 
+    // ReSharper disable once UnusedParameterInPartialMethod
     partial void OnFilterStringChanged(string value) => filter.OnNext(CreateFilter());
 
     private bool IsSelectedContact => SelectedContact != null;
 
     [RelayCommand(CanExecute = nameof(IsSelectedContact))]
-    private void OnDelete()
-    {
-        _ = backingContacts.Remove(SelectedContact!);
-    }
+    private void OnDelete() => _ = backingContacts.Remove(SelectedContact!);
 
     [RelayCommand(CanExecute = nameof(IsSelectedContact))]
     private void OnUpdate()
     {
-        if (TbName != null && TbSurName != null)
+        if (!string.IsNullOrWhiteSpace(TbName) && !string.IsNullOrWhiteSpace(TbSurName))
         {
             SelectedContact!.Name = TbName;
             SelectedContact.SurName = TbSurName;
@@ -88,31 +84,25 @@ public partial class MainWindowViewModel : ObservableObject, IDisposable
     }
 
     [RelayCommand(CanExecute = nameof(IsNameNotNull))]
-    private void OnCreate()
-    {
-        // We are sure TbName and TbSurName can not be null here
-        backingContacts.Add(new ObservableContact(TbName!, TbSurName!));
-    }
+    private void OnCreate() => backingContacts.Add(new ObservableContact(TbName!, TbSurName!));
 
-    private static List<ObservableContact> LoadData()
+    protected virtual void Dispose(bool disposing)
     {
-        IAssetLoader? assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
-        using Stream contactsOnDisk = assets!.Open(new System.Uri("avares://CRUD/Assets/MOCK_DATA.csv"));
-        using StreamReader reader = new(contactsOnDisk);
-        List<ObservableContact> contacts = new();
-        string? line;
-        while ((line = reader.ReadLine()) != null)
+        if (!disposedValue)
         {
-            string[] contact = line.Split(',');
-            contacts.Add(new ObservableContact(contact[0], contact[1]));
+            if (disposing)
+            {
+                backingContacts.Dispose();
+                list.Dispose();
+                filter.Dispose();
+            }
+            disposedValue = true;
         }
-        return contacts;
     }
 
     public void Dispose()
     {
-        list.Dispose();
-        filter.Dispose();
-        backingContacts.Dispose();
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
