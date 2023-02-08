23f72724dd1f185d69335968e44eb57631e6d907
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-02-08 12:09:11 +0530

    Add WIP CircleDrawer Example

---
+++ CircleDrawer/ViewLocator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel;
+
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace CircleDrawer;
+public class ViewLocator : IDataTemplate
+{
+    public Control Build(object? data)
+    {
+        string name = data!.GetType().FullName!.Replace("ViewModel", "View");
+        Type? type = Type.GetType(name);
+
+        return type != null ? (Control)Activator.CreateInstance(type)! : new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data) => data is INotifyPropertyChanged;
+}
---
+++ CircleDrawer/ViewModels/CircleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CircleDrawer.ViewModels;
+
+public partial class CircleViewModel : ObservableObject
+{
+    public double Diameter { get; set; }
+
+    public double X => CenterX - (Diameter / 2.0);
+
+    public double Y => CenterY - (Diameter / 2.0);
+
+    public double CenterX { get; set; }
+    public double CenterY { get; set; }
+
+    [ObservableProperty]
+    private string fillColor = "White";
+}
