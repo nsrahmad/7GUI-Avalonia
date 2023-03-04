af52c4e7b8ce511eec4db75a5c8c8ec2a8765580
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-03-04 08:06:42 +0530

    Add MouseCoordinatesToTuple converter
    This Converter is used to convert Mouse coordinates on canvas to Simple Tuple<double,double> and passes it to Command in ViewModel.

---
+++ CircleDrawer/Converters/MouseCoordinatesToTuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Input;
+
+namespace CircleDrawer.Converters;
+public class MouseCoordinatesToTuple : IValueConverter
+{
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var e = value as PointerPressedEventArgs;
+
+        if (e!.Source is Canvas canvas)
+        {
+            e!.Handled = true;
+            return new Tuple<double, double>(e!.GetPosition(canvas).X, e!.GetPosition(canvas).Y);
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
