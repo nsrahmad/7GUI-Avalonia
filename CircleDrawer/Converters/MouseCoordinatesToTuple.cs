9b28411c8ce36c898b285a2edd69c8361de1fcb7
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-04-25 12:05:11 +0530

    Minor cosmetic tweaks

--- CircleDrawer/Converters/MouseCoordinatesToTuple.cs
+++ CircleDrawer/Converters/MouseCoordinatesToTuple.cs
@@ -8,17 +8,13 @@ using Avalonia.Input;
 namespace CircleDrawer.Converters;
 public class MouseCoordinatesToTuple : IValueConverter
 {
-
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        var e = value as TappedEventArgs;
+        TappedEventArgs? e = value as TappedEventArgs;
         return e!.Source is Canvas canvas
             ? new Tuple<double, double>(e.GetPosition(canvas).X, e.GetPosition(canvas).Y)
             : null;
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        return value;
-    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => value;
 }
